164409c481e7f3846e14fa1a3c4acda6844102f8
fods811 <ngfods811@gmail.com>
2025-03-06 14:39:26 +0300

    SIZ-1846 [fix] - fixed jpg files

--- apps/next/src/views/main/ui/navigation-blocks/navigation-blocks.tsx
+++ apps/next/src/views/main/ui/navigation-blocks/navigation-blocks.tsx
@@ -1,14 +1,14 @@
 'use client';
 
-import imgContacts from '@/shared/assets/images/IMG_5291.jpg';
-import imgVideo from '@/shared/assets/images/IMG_6359.jpg';
-import imgPortfolio from '@/shared/assets/images/IMG_6422.jpg';
+import imgVideo from '@/shared/assets/images/1.jpg';
+import imgContacts from '@/shared/assets/images/2.jpg';
+import imgPortfolio from '@/shared/assets/images/3.jpg';
 import actorBlack from '@/shared/assets/lotties/actor_black.json';
 import contactBlack from '@/shared/assets/lotties/contact_black.json';
 import horizontalBlack from '@/shared/assets/lotties/Horizontal_black.json';
 import photosBlack from '@/shared/assets/lotties/photos_black.json';
 import verticalBlack from '@/shared/assets/lotties/Vertical_black.json';
-import CustomLink from '@/views/main/ui/navigation-blocks/ui/photo-link';
+import { PhotoLink } from '@/views/main/ui/navigation-blocks/ui/photo-link';
 
 export function NavigationBlocks() {
   const links = [
@@ -51,16 +51,14 @@ export function NavigationBlocks() {
 
   return (
     <section className="space-y-0 mt-20">
-      {links.map((link, index) => (
-        <CustomLink
-          key={index}
+      {links.map((link) => (
+        <PhotoLink
+          key={link.href}
           href={link.href}
           imageSrc={link.imageSrc}
           imageAlt={link.imageAlt}
           animationData={link.animationData}
-        >
-          {link.text}
-        </CustomLink>
+        />
       ))}
     </section>
   );
--- apps/next/src/views/main/ui/navigation-blocks/ui/photo-link.tsx
+++ apps/next/src/views/main/ui/navigation-blocks/ui/photo-link.tsx
@@ -1,39 +1,31 @@
 import React, { useState } from 'react';
+import type { LottieOptions } from 'lottie-react';
 import dynamic from 'next/dynamic';
 import Image, { type StaticImageData } from 'next/image';
 import Link from 'next/link';
 
-// Динамический импорт Lottie с отключением SSR
 const Lottie = dynamic(() => import('lottie-react'), { ssr: false });
 
-interface LinkProps {
+interface Props {
   href: string;
-  children: React.ReactNode;
-  className?: string;
   imageSrc: StaticImageData;
   imageAlt: string;
-  animationData: Record<string, unknown>;
+  animationData: LottieOptions['animationData'];
 }
 
-const CustomLink: React.FC<LinkProps> = ({
-  href,
-  className,
-  imageSrc,
-  imageAlt,
-  animationData,
-}) => {
+export const PhotoLink = ({ href, imageSrc, imageAlt, animationData }: Props) => {
   const [isAnimating, setIsAnimating] = useState(false);
 
   return (
-    <Link href={href} className={className}>
+    <Link href={href}>
       <div className="relative w-full h-[350px] md:h-[871px]">
-          <Image
-            src={imageSrc}
-            alt={imageAlt}
-            layout="fill"
-            objectFit="cover"
-            className="object-cover"
-          />
+        <Image
+          src={imageSrc}
+          alt={imageAlt}
+          layout="fill"
+          objectFit="cover"
+          className="object-cover"
+        />
         <div
           className="absolute inset-0 bg-black/30 flex items-center"
           onMouseEnter={() => setIsAnimating(true)}
@@ -45,5 +37,3 @@ const CustomLink: React.FC<LinkProps> = ({
     </Link>
   );
 };
-
-export default CustomLink;
