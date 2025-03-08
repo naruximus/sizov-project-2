abf164e7f7967804ea15f874a7022922a00f20c2
Danila Grebnev <naruximus@icloud.com>
2025-03-08 14:41:47 +0400

    feat(main): Add VideosSection component to main page (#11)

---
+++ apps/next/src/entities/integrated-videos/integrated-videos.model.ts
@@ -0,0 +1,15 @@
+import { z } from 'zod';
+
+export const IntegratedVideo = z.object({
+  id: z.number(),
+  name: z.string(),
+  source: z.string(),
+  description: z.string().optional(),
+  timing: z.string().optional(),
+  documentId: z.string(),
+  createdAt: z.string(),
+  updatedAt: z.string(),
+  publishedAt: z.string(),
+});
+
+export type TIntegratedVideo = z.infer<typeof IntegratedVideo>;
---
+++ apps/next/src/views/main/ui/videos-section/videos-section.tsx
@@ -0,0 +1,59 @@
+'use client';
+
+import { useState } from 'react';
+import clsx from 'clsx';
+import { Clapperboard } from 'lucide-react';
+
+import { TIntegratedVideo } from '@/entities/integrated-videos';
+
+interface Props {
+  videos: TIntegratedVideo[];
+}
+
+export function VideosSection({ videos }: Props) {
+  const [activeVideo, setActiveVideo] = useState(videos[0]);
+
+  return (
+    <section className="flex h-[500px] flex-col-reverse lg:flex-col ">
+      <div className="w-full h-full flex bg-stone-900 flex-col lg:flex-row ">
+        <iframe
+          className="w-full h-full inset-0 m-auto"
+          src={activeVideo.source}
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        />
+        <div className="flex flex-col overflow-scroll w-full lg:w-[360px]">
+          <div className="p-4 border-b border-stone-600">
+            <h3 className="text-lg">My projects</h3>
+            <p className="flex items-center gap-2 text-sm text-gray-500">
+              <Clapperboard />
+              {videos.length} videos
+            </p>
+          </div>
+          {videos.map((video, index) => (
+            <div
+              key={video.id}
+              className={clsx(
+                activeVideo.id === video.id ? 'bg-stone-800' : 'bg-stone-900',
+                'px-4 py-8 flex items-center w-full h-[100px] cursor-pointer hover:bg-stone-800 border-b border-stone-600',
+              )}
+              onClick={() => setActiveVideo(video)}
+            >
+              <div className="flex items-baseline w-full  gap-2 text-sm">
+                <span>{++index}</span>
+                <span className="flex-1 text-sm">{video.name}</span>
+                <span className="text-sm">{video?.timing}</span>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="py-4 gap-2">
+        <h3 className="text-lg font-bold">{activeVideo.name}</h3>
+        <p className="text-sm text-gray-500">{activeVideo.description}</p>
+      </div>
+    </section>
+  );
+}
