5c1674708bca6386392d51111ad016e71b47f670
naruximus <naruximus@icloud.com>
2025-05-24 22:06:52 +0400

    feat: add metadata for 404 page and update padding in NotFound component

--- apps/next/src/views/404/404.tsx
+++ apps/next/src/views/404/404.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 export const NotFound = () => {
   return (
-    <div className="flex flex-1 w-full items-center justify-center p-4 sm:p-8">
+    <div className="flex flex-1 w-full items-center justify-center p-4 sm:p-12">
       <div className="text-center space-y-4 w-full max-w-xl">
         <h1 className="text-4xl font-bold">404</h1>
         <p className="text-xl">Страница не найдена</p>
